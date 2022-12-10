9e999688e06ead71a8b08c69b6dba371742fd252
ShivkumarKonade <shivkumar.konade@virtoustack.om>
2022-12-10 21:51:31 +0530

    created

---
+++ lib/ui/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const gray = Color(0xFF4E4E4E);
+  static const lightGray = Color(0xFFC4C4C4);
+  static const primary = Color(0xFF25A0B0);
+  static const themeColor = Color(0xFFE1F6F4);
+  static const Color pink = Color(0xFFF5C8C6);
+  static const Color accentLine = Color(0xFFEEF2E2);
+
+  static const Gradient gradient = RadialGradient(
+    colors: [
+      Color(0xFFFFE1E0),
+      Color(0xFFE1F6F4),
+    ],
+    center: Alignment.bottomRight,
+    radius: 56,
+  );
+
+}
---
+++ lib/utils/assets.dart
@@ -0,0 +1,19 @@
+class Assets {
+  static const buttonShape = "assets/button_shape.svg";
+
+  static homeImage(int i) => "assets/home_image$i.png";
+
+  static String image(int i) => "assets/image$i.png";
+
+  static const rectangleBg = "assets/rectangle_bg.svg";
+  static const squareBg = "assets/square_bg.svg";
+  static const vector = "assets/vector.svg";
+
+  static String profile(int i) => "assets/profiles/profile$i.png";
+}
+
+class IconAsset {
+  static const chat = "assets/icons/chat.svg";
+  static const home = "assets/icons/home.svg";
+  static const menu = "assets/icons/menu.svg";
+}
