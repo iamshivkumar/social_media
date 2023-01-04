db92e0143b3c2eecafbeb2de0b74b1dc8b1e3f53
ShivkumarKonade <shivkumar.konade@virtoustack.om>
2023-01-04 10:47:17 +0530

    added labels

---
+++ lib/utils/labels.dart
@@ -0,0 +1,14 @@
+class Labels {
+  static const feed = "Feed";
+  static const followers = "Followers";
+  static const follows = "Follows";
+  static hrsAgo(int value) => "$value hrs ago";
+  static const messages = "Messages";
+  static const next = "Next";
+  static const online = "Online";
+  static const posts = "Posts";
+    static const searchForContacts = "Search for contacts";
+  static const socially = "Socially";
+  static const welcomeTo = "Welcome to";
+  static const writeAMessage = "Write a message...";
+}
